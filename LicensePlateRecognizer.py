9e340b2f99ea14c287bf91250f0440e40c93b43e
Syed Nahin Hossain <50939853+NAHIN-JZS@users.noreply.github.com>
2021-07-05 15:48:31 +0600

    Update LicensePlateRecognizer.py
    In this commit I have make sure of GPU usage

--- LicensePlateRecognizer.py
+++ LicensePlateRecognizer.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 23 15:02:36 2021
+Created on Sun Jul  4 23:29:53 2021
 
 @author: Nahin
 """
-
 
 from tkinter import *
 from tkinter.messagebox import askyesno, showinfo
@@ -66,7 +65,11 @@ def openfiledirectory():
         r_label.config(text = text)
         img = cv2.imread(fname)
         cv2.imshow("Selected Image",img)
-    
+    #img = Image.open(fname)
+    #img.thumbnail((800, 600))
+    #img = ImageTk.PhotoImage(img)
+    #label1.config(image=img)
+    #label1.image = img
 
 def openvideodirectory():
     global video
@@ -81,6 +84,33 @@ def openvideodirectory():
 
 def startanalyzing():
     showinfo("Info", "Analyzing....\nPress OK to continue")
+    net = cv2.dnn.readNet("yolov4-obj_6000.weights" , "yolov4-custom.cfg")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    
+    char_net = cv2.dnn.readNet("yolov4-obj_50000_char_final.weights" , "yolov4-obj_char_final.cfg")
+    char_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    char_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    
+    #Name custom object
+    classes = ["number_plate"]
+    
+    
+    char_classes = ["0","1","2","3","4","5","6","7","8","9","BA","CA","CHATTOGRAM","DA","DHAKA","GA","GHA","HA","JA","JASHORE","KA","KHA","KHULNA","METRO","NA","TA","VA"]
+    num_class = ["0","1","2","3","4","5","6","7","8","9"]
+    
+    
+    
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    #colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    colors = (0,255,0)
+    
+    
+    char_layer_names = char_net.getLayerNames()
+    char_output_layers = [char_layer_names[i[0] - 1] for i in char_net.getUnconnectedOutLayers()]
+    char_colors = np.random.uniform(0, 255, size=(len(char_classes), 3))
+    
     global video
     global fname
     if(video == 1):
@@ -88,15 +118,19 @@ def startanalyzing():
         #cv2.namedWindow("Selected Video", cv2.WINDOW_NORMAL)
         while True:
             _, img = cap.read()
+            if(img.size == 0):
+                showinfo("Complete", "Done\nPlease check output.csv file")
+                break
             img = cv2.resize(img, None , fx=0.4, fy=0.4)
             #cv2.imshow("Selected Video", img)
+            
             if cv2.waitKey(1)==27:
                 break
-            startmodel(img)
+            startmodel(img,net,char_net,classes,char_classes,num_class,layer_names,output_layers,colors,char_layer_names,char_output_layers,char_colors)
         cv2.destroyAllWindows()
     else:
         img = cv2.imread(fname)
-        startmodel(img)
+        startmodel(img,net,char_net,classes,char_classes,num_class,layer_names,output_layers,colors,char_layer_names,char_output_layers,char_colors)
         cv2.destroyAllWindows()
     text="NO IMAGE OR VIDEO IS SELECTED"
     r_label.config(text = text)
@@ -111,45 +145,21 @@ def showOutputOnInterfce(head,number):
     r_label.config(text = result)
     
 
-def startmodel(img):
+def startmodel(img,net,char_net,classes,char_classes,num_class,layer_names,output_layers,colors,char_layer_names,char_output_layers,char_colors):
     
+    start_time = time.clock()
     
-    
-    net = cv2.dnn.readNet("yolov4-obj.weights" , "yolov4-custom.cfg")
-    
-    char_net = cv2.dnn.readNet("yolov4-obj_char_final.weights" , "yolov4-obj_char_final.cfg")
-    
-    #Name custom object
-    classes = ["number_plate"]
-    
-    
-    char_classes = ["0","1","2","3","4","5","6","7","8","9","BA","CA","CHATTOGRAM","DA","DHAKA","GA","GHA","HA","JA","JASHORE","KA","KHA","KHULNA","METRO","NA","TA","VA"]
-    num_class = ["0","1","2","3","4","5","6","7","8","9"]
-    
-    
-    
-    
-    layer_names = net.getLayerNames()
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-    
-    
-    char_layer_names = char_net.getLayerNames()
-    char_output_layers = [char_layer_names[i[0] - 1] for i in char_net.getUnconnectedOutLayers()]
-    char_colors = np.random.uniform(0, 255, size=(len(char_classes), 3))
-    
-    
-    
-    
-    
+    #img = imutils.resize(img, width=416, height=416, inter=cv2.INTER_CUBIC)
     height, width, channels = img.shape
-
+    
     #detecting objects
-    blob = cv2.dnn.blobFromImage(img, 0.00392, (416,416), (0,0,0), True, crop = False)
-  
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (128,128), (0,0,0), True, crop = False)
+    
+    #tt=time.clock()
     net.setInput(blob)
+    
     outs = net.forward(output_layers)
-  
+    #print(time.clock()-tt)
   
 
     #Showing informations on the screen
@@ -166,7 +176,6 @@ def startmodel(img):
         confidence = scores[class_id]
         if confidence > 0.5:
         #object detection
-        #print(class_id)
         
           center_x = int (detection[0] * width)
           center_y = int (detection[1] * height)
@@ -190,7 +199,6 @@ def startmodel(img):
 
 
     font = cv2.FONT_HERSHEY_PLAIN
-    k=0
     for i in range(len(boxes)):
       if i in indexes:
         
@@ -202,30 +210,39 @@ def startmodel(img):
           new_img = img[y:y+h, x:x+w]
           if new_img.size == 0:
               continue
+          #print(img)
+          ###########################################
+          #new_img = imutils.resize(new_img, width=416, height=416, inter=cv2.INTER_CUBIC)
+        
           
-        
-        
           cv2.rectangle(img, (x,y), (x+w,y+h), color, 2)
           cv2.putText(img, labe +" "+str(plate_confi)[:4], (x,y), font, 0.8, (0, 255, 0), 1)
           
-          cv2.imshow('Selected Video with Detected License Plate',img)
-          cv2.waitKey(1)
+          #cv2.imshow('Selected Video with Detected License Plate',img)
+          
+          #cv2.waitKey(1)
+          
         
-        
+          
            
           #Charecter part
           #loading image
-          
+          #img_name = img_path[43:-4]
           char_img = new_img
-          char_img = cv2.resize(char_img, None , fx=1.0, fy=1.0)
+          #char_img = cv2.resize(char_img, None , fx=2, fy=2)
+          
+          #char_img=cv2.detailEnhance(char_img, 10, 0.99)
+          
           char_height, char_width, char_channels = char_img.shape
         
           #detecting objects
-          char_blob = cv2.dnn.blobFromImage(char_img, 0.00392, (416,416), (0,0,0), True, crop = False)
+          char_blob = cv2.dnn.blobFromImage(char_img, 0.00392, (256,256), (0,0,0), True, crop = False)
           
+          #tt=time.clock()
           char_net.setInput(char_blob)
-          char_outs = char_net.forward(char_output_layers)
           
+          char_outs = char_net.forward(char_output_layers)
+          #print(time.clock()-tt)
           
         
           #Showing informations on the screen
@@ -242,7 +259,7 @@ def startmodel(img):
               char_confidence = char_scores[char_class_id]
               if char_confidence > 0.9:
                 #object detection
-                
+                #print(class_id)
                 
                 char_center_x = int (char_detection[0] * char_width)
                 char_center_y = int (char_detection[1] * char_height)
@@ -263,10 +280,6 @@ def startmodel(img):
           #print(indexes)
         
         
-        
-        
-          char_font = cv2.FONT_HERSHEY_PLAIN
-          
           char_final_obj = []
           for i in range(len(char_boxes)):
             if i in char_indexes:
@@ -280,32 +293,38 @@ def startmodel(img):
                 #print(obj_index)
                 char_final_obj.append(char_obj_index)
                 char_label = str(char_classes[char_class_ids[i]])
-                char_color = char_colors[char_class_ids[i]]
+                
               
               
                 char_confi = char_confidences[i]
               
               
-                char_new_img = char_img[y:y+h, x:x+w]
-                if new_img.size == 0:
-                    continue
+                #char_new_img = char_img[y:y+h, x:x+w]
+                #if new_img.size == 0:
+                    #continue
                 #print(img)
-                char_new_img = imutils.resize(char_new_img, width=700, height=400, inter=cv2.INTER_CUBIC)
+                #char_new_img = imutils.resize(char_new_img, width=700, height=400, inter=cv2.INTER_CUBIC)
                 
-        
+                
                 cv2.rectangle(char_img, (x,y), (x+w,y+h), color, 2)
                 cv2.putText(char_img, char_label+" "+str(char_confi)[0:4], (x,y), font, 1, (0, 0, 255), 1)
                 
-                cv2.imshow('Plate Recognizer',char_img)
+                
+               
+                
+                
+                
+                cv2.imshow('Selected Video with Detected License Plate',img)
+                
+                #cv2.imshow('Plate Recognizer',char_img)
                 cv2.waitKey(1)
           
           char_final_obj.sort()
-          #print(final_obj)
+          
           number=" "
           high = " "
           for j in char_final_obj:
               char_index_no = j[4]
-              
               
               if str(char_classes[char_class_ids[char_index_no]]) in num_class:
                   
@@ -314,17 +333,36 @@ def startmodel(img):
                   high = high + char_classes[char_class_ids[char_index_no]]+ "  "
           localtime = time.asctime( time.localtime(time.time()) )
           lst = [high,number,localtime]
+          
+          
+          
           with open('Output.csv','a') as file:
               w_obj = csv.writer(file)
               w_obj.writerow(lst)
           
           print("place is : " + high )
-          print("number "+ number) 
+          print("number "+ number)
+          
           if(video==0):
               showOutputOnPopUp(high,number)
           else:
               showOutputOnInterfce(high,number)
+          
+          end_time = time.clock()
+          t_time = end_time-start_time
+          print(t_time)
+          ms_per_image = t_time * 1000 / 100
+          print("Time per inference: %f ms" % (ms_per_image))
+          print("FPS: ", 1/t_time)
+    #cv2.imshow("car",img)        
+                       
+    #key = cv2.waitKey(1) & 0xFF
+    #if key == ord('q'):
+     #   break      
+
         
+    #cv2.destroyAllWindows()
+  #cap.release()        
 
 
 
